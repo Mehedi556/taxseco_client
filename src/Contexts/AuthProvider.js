4cf94458cef2d68ded4d66d950c561e165b73755
mehedi556 <mehe556di@gmail.com>
2022-11-10 08:28:26 +0600

    adding authprovider

--- src/Contexts/AuthProvider.js
+++ src/Contexts/AuthProvider.js
@@ -9,7 +9,41 @@ const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
 
+    const [user , setUser] = useState(null);
+    const [loading , setLoading] = useState(true);
 
+    const createUser = (email , password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth , email , password);
+    }
+
+    const signIn = (email , password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth , email , password);
+    }
+    const signUpFacebook = (provider) => {
+        return signInWithPopup(auth , provider);
+    }
+
+
+
+
+
+useEffect( () => {
+    const unsubscribe = onAuthStateChanged(auth , (currentUser) => {
+        setUser(currentUser);
+        setLoading(false);
+    });
+    return () => {
+        unsubscribe();
+    }
+} , [])
+
+const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+
+}
 
 
     const authInfo = { user , createUser , signIn , logOut , signUpFacebook}
