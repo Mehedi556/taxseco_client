80542755077e509b51d38f2d8f0df14190838486
mehedi556 <mehe556di@gmail.com>
2022-11-10 08:13:57 +0600

    adding services page

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -6,7 +6,11 @@ const Services = () => {
     const services = useLoaderData();
     console.log(services);
     return (
-        
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-14'>
+    {
+        services.map(service => <SingleService key={service._id} service={service}></SingleService>)
+    }
+</div>
     );
 };
 
