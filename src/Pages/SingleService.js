dfe4beac7f2b9c4b0aed566bedfde8eee3089cac
mehedi556 <mehe556di@gmail.com>
2022-11-10 08:12:17 +0600

    adding single service card

--- src/Pages/SingleService.js
+++ src/Pages/SingleService.js
@@ -5,7 +5,21 @@ import { PhotoProvider, PhotoView } from 'react-photo-view';
 const SingleService = ({service}) => {
     const {_id , name , Price , picture} = service;
   return (
-   
+    <div className='border border-solid border-slate-400 rounded-xl p-3'>
+           
+    <PhotoProvider>
+      <PhotoView src={picture}>
+        <img src={picture} alt="" />
+      </PhotoView>
+    </PhotoProvider>
+
+                <div className='text-left py-3 pb-6'>
+                <h1><span className='font-bold'>Name: </span>{name}</h1>
+                <h1><span className='font-bold'>Per Day: </span>{Price}$</h1>
+                </div>
+                <button className="btn btn-ghost btn-outline w-[100%] justify-center">View Details</button>
+
+        </div>
   );
 };
 
