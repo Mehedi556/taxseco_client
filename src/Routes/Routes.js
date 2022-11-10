efbfb46eb160792f7469acef80f789b7f8f2e973
mehedi556 <mehe556di@gmail.com>
2022-11-10 08:10:42 +0600

    adding routes

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -9,3 +9,46 @@ import MyReviews from "../Pages/MyReviews";
 import Register from "../Pages/Register";
 import Services from "../Pages/Services";
 
+export const routes = createBrowserRouter([
+  {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+          {
+              path: '/',
+              element: <Home></Home>,
+              loader: () => fetch('http://localhost:5000/services')
+          },
+          {
+            path: '/login',
+            element: <Login></Login>,
+          },
+          {
+            path: '/register',
+            element: <Register></Register>,
+          },
+          {
+            path: '/blog',
+            element: <Blog></Blog>
+          },
+          {
+            path: '/myreviews',
+            element: <MyReviews></MyReviews>
+          },
+          {
+            path: '/addservice',
+            element: <AddService></AddService>
+          },
+          {
+            path: '/service',
+            element: <Services></Services>,
+            loader: () => fetch('http://localhost:5000/service')
+          },
+          {
+            path: '*',
+            element: <Error></Error>,
+          }
+      ]
+  }
+])
+
